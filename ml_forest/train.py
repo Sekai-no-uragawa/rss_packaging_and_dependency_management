6bddf253f1312cd5c99e31483534cbd6f139d04e
Mikhail Kostin <1bitt2@mail.ru>
2022-05-01 23:57:41 +0300

    Add model type selection

--- ml_forest/train.py
+++ ml_forest/train.py
@@ -1,10 +1,39 @@
+from calendar import c
 from pathlib import Path
 
 import click
+from click import Context as ctx
 import pandas as pd
 
 from .ClassifierSwitcher import ClfSwitcher
 from .data import get_dataset
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+def create_pipeline(
+    clf_type
+):
+    print(type(clf_type))
+#     use_scaler: bool, max_iter: int, logreg_C: float, random_state: int
+# ) -> Pipeline:
+#     pipeline_steps = []
+#     if use_scaler:
+#         pipeline_steps.append(("scaler", StandardScaler()))
+#     pipeline_steps.append(
+#         (
+#             "classifier",
+#             LogisticRegression(
+#                 random_state=random_state, max_iter=max_iter, C=logreg_C
+#             ),
+#         )
+#     )
+#     return Pipeline(steps=pipeline_steps)
+
+
 
 @click.command()
 @click.option(
@@ -32,14 +61,26 @@ from .data import get_dataset
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
     show_default=True,
 )
+@click.option(
+    '--clf-type',
+    type=click.Choice(['KNeighborsClassifier', 'DecisionTreeClassifier']),
+    default='DecisionTreeClassifier'
+)
 def train(
     dataset_path: Path,
     random_state,
     test_size,
     save_model_path: Path,
+    clf_type
 ) -> None:
     features_train, features_val, target_train, target_val = get_dataset(
         dataset_path,
         random_state,
         test_size,
     )
+
+    mapping_dict = {
+        'KNeighborsClassifier' : KNeighborsClassifier(),
+        'DecisionTreeClassifier' : DecisionTreeClassifier(),
+    }
+    create_pipeline(mapping_dict[clf_type])
