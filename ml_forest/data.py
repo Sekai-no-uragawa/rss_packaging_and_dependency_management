871b9a07aaec9638a8ed946349aeb86b55b94677
Mikhail Kostin <1bitt2@mail.ru>
2022-05-01 22:47:05 +0300

    Error correction

--- ml_forest/data.py
+++ ml_forest/data.py
@@ -12,15 +12,15 @@ def get_dataset(
     test_split_ratio: float,
 ) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     dataset = pd.read_csv(csv_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
-    features = dataset.drop("target", axis=1)
-    target = dataset["target"]
+    click.echo(f"\nDataset shape: {dataset.shape}.")
+    features = dataset.drop("Cover_Type", axis=1)
+    target = dataset["Cover_Type"]
     features_train, features_val, target_train, target_val = train_test_split(
         features, target, test_size=test_split_ratio, random_state=random_state
     )
     click.echo(
-        f"features_train shape: {features_train.shape}.\n \
-        features_val shape: {features_val.shape}.\n \
-        target_train shape: {target_train.shape}.\n \
-        target_val shape: {target_val.shape}.\n")
+        f"\nfeatures_train shape: {features_train.shape}.\n\
+features_val shape: {features_val.shape}.\n\
+target_train shape: {target_train.shape}.\n\
+target_val shape: {target_val.shape}.\n")
     return features_train, features_val, target_train, target_val
