f89599648e5a9d8def7565fb80634184877957d8
Mikhail Kostin <1bitt2@mail.ru>
2022-05-01 22:48:06 +0300

    Add data split and CLI options

--- ml_forest/train.py
+++ ml_forest/train.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 import click
 import pandas as pd
-from pip import main
+
+from .ClassifierSwitcher import ClfSwitcher
+from .data import get_dataset
 
 @click.command()
 @click.option(
@@ -11,11 +13,33 @@ from pip import main
     default="data/train.csv",  
     type=click.Path(exists=True, dir_okay=False, path_type=Path)
 )
+@click.option(
+    "-s",
+    "--save-model-path",
+    default="profile_report.html",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--test-size",
+    default=0.2,
+    type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
+)
 def train(
-    dataset_path: Path
+    dataset_path: Path,
+    random_state,
+    test_size,
+    save_model_path: Path,
 ) -> None:
-    dataset = pd.read_csv(dataset_path)
-    click.echo(f"Dataset shape: {dataset.shape}.")
-
-if __name__ == '__main__':
-    train()
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_size,
+    )
