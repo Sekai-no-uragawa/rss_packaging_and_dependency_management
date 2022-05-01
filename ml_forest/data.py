0111f5c2891d83561fab4a8676256dad1ca0def8
Mikhail Kostin <1bitt2@mail.ru>
2022-05-01 22:22:31 +0300

    create train test split script

---
+++ ml_forest/data.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Tuple
+
+import click
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_dataset(
+    csv_path: Path,
+    random_state: int,
+    test_split_ratio: float,
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
+    dataset = pd.read_csv(csv_path)
+    click.echo(f"Dataset shape: {dataset.shape}.")
+    features = dataset.drop("target", axis=1)
+    target = dataset["target"]
+    features_train, features_val, target_train, target_val = train_test_split(
+        features, target, test_size=test_split_ratio, random_state=random_state
+    )
+    click.echo(
+        f"features_train shape: {features_train.shape}.\n \
+        features_val shape: {features_val.shape}.\n \
+        target_train shape: {target_train.shape}.\n \
+        target_val shape: {target_val.shape}.\n")
+    return features_train, features_val, target_train, target_val
