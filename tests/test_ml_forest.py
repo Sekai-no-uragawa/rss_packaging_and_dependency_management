e07723dc8039475d22f298e8cd3a0c77a8f749ea
Mikhail Kostin <1bitt2@mail.ru>
2022-05-09 23:48:56 +0300

    feat: add test for train

--- tests/test_ml_forest.py
+++ tests/test_ml_forest.py
@@ -1,6 +1,27 @@
 from ml_forest import __version__
+from click.testing import CliRunner
+import pytest
 
+from ml_forest.train import train
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
 
 def test_version():
     assert __version__ == '0.1.0'
 
+def test_train(
+    runner: CliRunner
+) -> None:
+    result = runner.invoke(
+        train,
+        [
+            "--clf-type",
+            'LogReg',
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Invalid value for '--clf-type'" in result.output
