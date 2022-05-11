6f94697369324e055390b4b618ae1f08cc8b1086
Mikhail Kostin <1bitt2@mail.ru>
2022-05-11 18:30:25 +0300

    feat: add test with data and filesystem isolation

--- tests/test_ml_forest.py
+++ tests/test_ml_forest.py
@@ -1,6 +1,9 @@
 from ml_forest import __version__
 from click.testing import CliRunner
+import click
 import pytest
+import pandas as pd
+from joblib import load
 
 from ml_forest.train import train
 
@@ -25,3 +28,32 @@ def test_train(
     )
     assert result.exit_code == 2
     assert "Invalid value for '--clf-type'" in result.output
+
+def test_train_isolated(
+    runner: CliRunner
+) -> None:
+    test_data_path = 'tests/test_data.csv'
+    pred_data_path = 'tests/test_data_to_predict.csv'
+    test_data = pd.read_csv(test_data_path)
+    pred_data = pd.read_csv(pred_data_path)
+    with runner.isolated_filesystem('C:/test'):
+        test_data.to_csv('test_data.csv')
+        pred_data.to_csv('pred_data.csv')
+        result = runner.invoke(
+            train,
+            [
+                '--dataset-path',
+                'test_data.csv'
+            ]
+        )
+        assert result.exit_code == 0
+        print(result.output)
+        assert 'Model saved to' in result.output
+
+        pred_data_path = 'pred_data.csv'
+        loaded_model = load('data/model.joblib')
+        pred_data = pd.read_csv(pred_data_path)
+        try:
+            result = loaded_model.predict(pred_data)
+        except Exception:
+            raise Exception('Error in model')
